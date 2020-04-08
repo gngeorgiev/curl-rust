e9ad8981509a4f8b155422439715d5799510ea3c
ggeorgiev_ <ggeorgiev@gitlab.com>
2020-04-09 00:47:49 +0300

    no socket

--- curl-sys/build.rs
+++ curl-sys/build.rs
@@ -212,7 +212,7 @@ fn main() {
             .file("curl/lib/vauth/vauth.c");
     }
 
-    if cfg!(not(windows)) {
+    if cfg!(any(target_os = "macos")) {
         cfg.define("USE_UNIX_SOCKETS", None)
             .define("HAVE_SYS_UN_H", None);
     }
